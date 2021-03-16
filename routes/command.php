0a8ef01e4206a2506aefd0a8f2024ba4dede0b7a
Marcos Di Paolo <marcos.dipaolo@softvision.com>
2021-03-16 11:48:10 -0300

    initial commit

---
+++ routes/command.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/migrate', function (Request $request) {
+    try {
+        \Artisan::call('migrate', ['--path' => 'database/migrations', '--force' => true]);
+        return response()->json(['message' => 'migrated'], 200);
+    } catch (\Exception $e) {
+        return response()->json($e->getMessage(), 500);
+    }
+});
+    
